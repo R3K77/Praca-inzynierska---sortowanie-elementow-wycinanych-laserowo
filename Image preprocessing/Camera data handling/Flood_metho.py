2f027cea706de5179ba2b2da7d7464d2325354a6
kszygenda <rszygenda2002@gmail.com>
2024-06-05 16:23:48 +0200

    Test do wdrozen

--- Image preprocessing/Camera data handling/Flood_metho.py
+++ Image preprocessing/Camera data handling/Flood_metho.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time # Tylko do printowania co jakiś czas
 
 
 def RefPoint_Detection(img, lower, upper):
@@ -11,16 +12,24 @@ def RefPoint_Detection(img, lower, upper):
     mask = cv2.inRange(hsv, lower, upper)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    avg_x = avg_y = 0
-    for contour in contours:
-        hull = cv2.convexHull(contour)
-        sum_x = sum_y = 0
-        for point in hull:
-            sum_x += point[0][0]
-            sum_y += point[0][1]
-        avg_x = sum_x / len(hull)
-        avg_y = sum_y / len(hull)
-    return avg_x, avg_y, mask
+
+    max_area = 0
+    max_area_index = -1
+    for i, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if area > max_area:
+            max_area = area
+            max_area_index = i
+
+    if max_area_index == -1:
+        return 0, 0, mask
+
+    # Obliczanie punktu środkowego dla największego konturu
+    M = cv2.moments(contours[max_area_index])
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+
+    return cX, cY, mask
 
 
 def Calculate_angle(base_x, base_y, xAxis_x, xAxis_y, yAxis_x, yAxis_y, rec_edge_points):
@@ -31,14 +40,14 @@ def Calculate_angle(base_x, base_y, xAxis_x, xAxis_y, yAxis_x, yAxis_y, rec_edge
     xAxis = np.array([xAxis_x, xAxis_y])
     # Wektor osi X
     vec_x = xAxis - base
-    vec_rec = rec_edge_points[1] - rec_edge_points[0]
+    vec_rec = rec_edge_points[3] - rec_edge_points[2]
     # Obliczanie kąta między wektorami
     angle_z = np.arctan2(np.linalg.det([vec_x, vec_rec]), np.dot(vec_x, vec_rec))
     angle_z = np.degrees(angle_z)
     # translacja
-    TR_x = rec_edge_points[1][0] - base_x
-    TR_y = rec_edge_points[1][1] - base_y
-    angle_z = 180 + angle_z
+    TR_x = rec_edge_points[2][0] - base_x
+    TR_y = rec_edge_points[2][1] - base_y
+    angle_z = angle_z
     return TR_x, TR_y, angle_z
 
 
@@ -61,12 +70,9 @@ def camera_calibration(frame):
 # Funkcja służy do wykrycia największego elementu na obrazie.
 # Warunek konieczny działania tej funkcji to jednolite tło, którego kolor nie jest podobny do koloru obiektu.
 # (np. blacha na jasnoszarym stole). Z racji na statyczne położenie kamery, można dodać
-
-# TODO - ponownie przetestować po update
-# W przypadku kiedy nie działa najlepiej, dodać WarpPerspective, przed flood aby zmniejszyć obszar roboczy systemu wizyjnego.
 def Workspace_detection(frame):
-    lowerDiff = [3, 2, 2, 5]
-    upperDiff = [3, 2, 2, 5]
+    lowerDiff = [3, 3, 3, 5]
+    upperDiff = [3, 3, 3, 5]
     birdEye = np.maximum(frame, 10)
     foreground = birdEye.copy()
 
@@ -79,11 +85,9 @@ def Workspace_detection(frame):
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
     thresh = cv2.threshold(blur, 1, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
-    cntrs, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)  # Tu można zamienić na simple, jeżeli ma być wykrywany prostokątny element
+    thresh2 = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)))
+    thresh3 = cv2.morphologyEx(thresh2, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)))
+    cntrs, _ = cv2.findContours(thresh3, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)  # Tu można zamienić na simple, jeżeli ma być wykrywany prostokątny element
 
     max_area = 0
     max_area_index = -1
@@ -97,10 +101,13 @@ def Workspace_detection(frame):
     # https://stackoverflow.com/questions/7263621/how-to-find-corners-on-a-image-using-opencv
     arc = 0.04 * cv2.arcLength(cntrs[max_area_index], True)
     poly = cv2.approxPolyDP(cntrs[max_area_index], arc, True)
+
     if len(poly) != 4:
         return birdEye, None, thresh, blur, gray, None, None, None
     if len(poly) == 4:
         cv2.drawContours(birdEye, [poly], 0, (0), 5)
+        cv2.drawContours(birdEye, cntrs, max_area_index, (0, 255, 0), 2)
+        poly = poly[np.argsort(poly[:, 0][:, 1])]
         # Rogi prostokąta
         left_upper_corner = poly[0][0]
         left_bottom_corner = poly[1][0]
@@ -110,12 +117,12 @@ def Workspace_detection(frame):
             (right_upper_corner[0] - left_upper_corner[0]) ** 2 + (right_upper_corner[1] - left_upper_corner[1]) ** 2))
         h = int(np.sqrt((right_upper_corner[0] - right_bottom_corner[0]) ** 2 + (
                     right_upper_corner[1] - right_bottom_corner[1]) ** 2))
-        cv2.circle(birdEye, left_bottom_corner, 5, (0, 0, 255), -1)
-        cv2.circle(birdEye, right_upper_corner, 5, (0, 0, 255), -1)
-        cv2.circle(birdEye, right_bottom_corner, 5, (0, 0, 255), -1)
-        cv2.circle(birdEye, left_upper_corner, 5, (0, 0, 255), -1)
+        cv2.circle(birdEye, left_bottom_corner, 5, (255, 0, 255), -1) # różowy
+        cv2.circle(birdEye, right_upper_corner, 5, (0, 0, 255), -1) # czerwony
+        cv2.circle(birdEye, right_bottom_corner, 5, (255, 255, 255), -1) # biały
+        cv2.circle(birdEye, left_upper_corner, 5, (0, 255, 255), -1) # żółty
         pts_org = np.array([left_upper_corner, right_upper_corner, right_bottom_corner, left_bottom_corner], np.float32)
-        pts_dst = np.array([[0, 0], [w, 0], [w, h], [0, h]], np.float32)
+        pts_dst = np.array([[0, 0], [w, 0],[w, h], [0, h]], np.float32)
         matrix = cv2.getPerspectiveTransform(pts_org, pts_dst)
         workplace = cv2.warpPerspective(birdEye, matrix, (w, h))
         rect = cv2.minAreaRect(pts_org)
@@ -124,7 +131,7 @@ def Workspace_detection(frame):
             angle = -(90 + angle)
         elif angle > 45:
             angle = -(90 - angle)
-        return birdEye, workplace, thresh, blur, gray, right_bottom_corner, angle, pts_org
+        return birdEye, workplace, thresh3, blur, gray, right_bottom_corner, angle, pts_org
 
 
 ## Dolna i górna granica kolorów
@@ -139,11 +146,13 @@ lower_green = np.array([34, 49, 154])
 upper_green = np.array([71, 255, 255])
 
 cap = cv2.VideoCapture(0)
-
+last_print_time = time.time()
 while True:
     rd, frame = cap.read()
     frame = camera_calibration(frame)
+    # Detekcja Workspace
     birdEye, workplace, thresh, blur, gray, _, angle, pts_org = Workspace_detection(frame)
+    # punkty układu bazowego + rysowanie
     base_x, base_y, mask_red = RefPoint_Detection(frame, lower_red, upper_red)
     xAxis_x, xAxis_y, mask_blue = RefPoint_Detection(frame, lower_blue, upper_blue)
     yAxis_x, yAxis_y, mask_green = RefPoint_Detection(frame, lower_green, upper_green)
@@ -152,27 +161,37 @@ while True:
     cv2.circle(birdEye, (int(yAxis_x), int(yAxis_y)), 5, (0, 0, 255), -1)  # Y-axis point in green
     TR_x, TR_y, angle_2 = Calculate_angle(base_x, base_y, xAxis_x, xAxis_y, yAxis_x, yAxis_y, pts_org)
 
+    # Linia do translacji
+    start_point = (int(base_x), int(base_y))
+    end_point = (int(base_x + TR_x), int(base_y + TR_y))
+    cv2.line(birdEye, start_point, end_point, (0, 255, 0), 2)
+
     cv2.imshow('thresh', thresh)
     cv2.imshow('blur', blur)
-    cv2.imshow('gray', gray)
-    cv2.imshow('mask_red', mask_red)
-    cv2.imshow('mask_blue', mask_blue)
-    cv2.imshow('mask_green', mask_green)
-    if workplace is not None:
-        cv2.imshow('workplace', workplace)
+    # cv2.imshow('gray', gray)
+    # if workplace is not None:
+    #     cv2.imshow('workplace', workplace)
 
     cv2.imshow('birdEye', birdEye)
-    print("---------------------------")
-    print(f'Angle: {angle}')
-    print(f'Angle2 : {angle_2}')
-    print(f"TR_x: {TR_x}")
-    print(f"TR_y: {TR_y}")
+    current_time = time.time()
+    if current_time - last_print_time >= 0.5:
+        print("---------------------------")
+        print(f'Angle: {angle}')
+        print(f'Angle2 : {angle_2}')
+        print(f"TR_x: {TR_x}")
+        print(f"TR_y: {TR_y}")
+        last_print_time = current_time
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+# TODO - translacja powinna być podana w mm, więc trzeba przeskalować na podstawie obsługiwanej blachy, może info z CSV?
+
+
+
 
 
 

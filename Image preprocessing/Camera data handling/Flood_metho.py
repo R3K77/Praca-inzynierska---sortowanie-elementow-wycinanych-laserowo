53226eb8295d432fb0e8ccee2cc9539321d1a7d8
RaV <rafal.szygenda@student.put.poznan.pl>
2024-06-05 14:40:03 +0200

    Update Flood_metho.py

--- Image preprocessing/Camera data handling/Flood_metho.py
+++ Image preprocessing/Camera data handling/Flood_metho.py
@@ -24,71 +24,72 @@ def camera_calibration(frame):
 # TODO - ponownie przetestować po update
 # W przypadku kiedy nie działa najlepiej, dodać WarpPerspective, przed flood aby zmniejszyć obszar roboczy systemu wizyjnego.
 def Workspace_detection(frame):
-    lowerDiff = [2, 2, 2, 5]
-    upperDiff = [2, 2, 2, 5]
+    lowerDiff = [3, 2, 2, 5]
+    upperDiff = [3, 2, 2, 5]
     birdEye = np.maximum(frame, 10)
     foreground = birdEye.copy()
-    seed = (10, 10)
-    cv2.floodFill(foreground, None, seedPoint=seed, newVal=(0, 0, 0), loDiff=lowerDiff, upDiff=upperDiff)
+
+    height, width = frame.shape[:2]
+    seeds = [(10, 10), (10, height-10), (width-10, 10), (width-10, height-10)]
+    for seed in seeds:
+        cv2.floodFill(foreground, None, seedPoint=seed, newVal=(0, 0, 0), loDiff=lowerDiff, upDiff=upperDiff)
 
     gray = cv2.cvtColor(foreground, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
     thresh = cv2.threshold(blur, 1, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (5,5)))
     cntrs, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Tu można zamienić na simple, jeżeli ma być wykrywany prostokątny element
 
     max_area = 0
-    max_area_index = 0
+    max_area_index = -1
     for i, cntr in enumerate(cntrs):
         area = cv2.contourArea(cntr)
         if area > max_area:
             max_area = area
             max_area_index = i
 
-
-    # kąt obrotu
-    x, y, w, h = cv2.boundingRect(cntrs[max_area_index])
-    rect = cv2.minAreaRect(cntrs[max_area_index])
-    (width_box, height_box), angle = rect[1], rect[2]
-
-    if width_box > height_box:
-        angle += 90
-
-    if cntrs[max_area_index][0][0][0] < cntrs[max_area_index][1][0][0]:
-        angle += 180
-
-    cv2.drawContours(birdEye, cntrs, max_area_index, (0,255,0), 2)
-
-    # Rogi prostokąta
-    left_upper_corner = (x, y)
-    left_bottom_corner = (x, y+h)
-    right_upper_corner = (x+w, y)
-    right_bottom_corner = (x+w, y+h)
-    cv2.circle(birdEye, left_upper_corner, 5, (0, 0, 255), -1)
-    cv2.circle(birdEye, left_bottom_corner, 5, (0, 0, 255), -1)
-    cv2.circle(birdEye, right_upper_corner, 5, (0, 0, 255), -1)
-    cv2.circle(birdEye, right_bottom_corner, 5, (0, 0, 255), -1)
-
-
-
-
-    pts_org = np.array([left_upper_corner, right_upper_corner, right_bottom_corner, left_bottom_corner], np.float32)
-    pts_dst = np.array([[0, 0], [w, 0], [w, h], [0, h]], np.float32)
-
-    matrix = cv2.getPerspectiveTransform(pts_org, pts_dst)
-    workplace = cv2.warpPerspective(birdEye, matrix, (w, h))
-
-    return birdEye, workplace, thresh, blur, gray, right_bottom_corner
+    # wyznaczenie rogów prostokąta
+    # https://stackoverflow.com/questions/7263621/how-to-find-corners-on-a-image-using-opencv
+    arc = 0.04 * cv2.arcLength(cntrs[max_area_index], True)
+    poly = cv2.approxPolyDP(cntrs[max_area_index], arc, True)
+    if len(poly) != 4:
+        return birdEye, None, thresh, blur, gray, None, None
+    if len(poly) == 4:
+        cv2.drawContours(birdEye, [poly], 0, (0), 5)
+        # Rogi prostokąta
+        left_upper_corner = poly[0][0]
+        left_bottom_corner = poly[1][0]
+        right_upper_corner = poly[2][0]
+        right_bottom_corner = poly[3][0]
+        w = int(np.sqrt((right_upper_corner[0] - left_upper_corner[0]) ** 2 + (right_upper_corner[1] - left_upper_corner[1]) ** 2))
+        h = int(np.sqrt((right_upper_corner[0] - right_bottom_corner[0]) ** 2 + (right_upper_corner[1] - right_bottom_corner[1]) ** 2))
+        cv2.circle(birdEye, left_bottom_corner, 5, (0, 0, 255), -1)
+        cv2.circle(birdEye, right_upper_corner, 5, (0, 0, 255), -1)
+        cv2.circle(birdEye, right_bottom_corner, 5, (0, 0, 255), -1)
+        pts_org = np.array([left_upper_corner, right_upper_corner, right_bottom_corner, left_bottom_corner], np.float32)
+        pts_dst = np.array([[0, 0], [w, 0], [w, h], [0, h]], np.float32)
+        matrix = cv2.getPerspectiveTransform(pts_org, pts_dst)
+        workplace = cv2.warpPerspective(birdEye, matrix, (w, h))
+        rect = cv2.minAreaRect(pts_org)
+        angle = rect[2]
+        if angle < -45:
+            angle = -(90 + angle)
+        elif angle > 45:
+            angle = -(90 - angle)
+        return birdEye, workplace, thresh, blur, gray, right_bottom_corner, angle
 
 
-cap = cv2.VideoCapture(0)
+
+
+cap = cv2.VideoCapture(1)
 
 while True:
     rd, frame = cap.read()
     frame = camera_calibration(frame)
-    birdEye, workplace, thresh, blur, gray, _ = Workspace_detection(frame)
+    birdEye, workplace, thresh, blur, gray, _, angle  = Workspace_detection(frame)
     cv2.imshow('thresh', thresh)
     cv2.imshow('blur', blur)
     cv2.imshow('gray', gray)
@@ -96,9 +97,10 @@ while True:
         cv2.imshow('workplace', workplace)
 
     cv2.imshow('birdEye', birdEye)
-
+    print(f'Angle: {angle}')
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
 cap.release()
 cv2.destroyAllWindows()

d90310f5528147e39526b64cc6c68b2b2da43615
kszygenda <rszygenda2002@gmail.com>
2024-10-18 11:35:12 +0200

    camera quality control test b4 implementation

--- Image preprocessing/Camera data handling/testy/System Wizyjny - kontrola jakości/GcodeExtraction/Element_pojedynczy.py
+++ Image preprocessing/Camera data handling/testy/System Wizyjny - kontrola jakości/GcodeExtraction/Element_pojedynczy.py
@@ -13,7 +13,7 @@ import random
 # - Rotacja translacja blachy TODO
 # - mocowanie do stolu TODO
 
-# TODO przebudowa funkcji do dynamicznej rozdzielczości pod kamere
+# przebudowa funkcji do dynamicznej rozdzielczości pod kamere DONE
 def allGcodeElementsCV2(sheet_path, scale = 3, arc_pts_len = 300):
     """
     Creates cv2 images of sheet elements from gcode.
@@ -109,11 +109,26 @@ def singleGcodeElementCV2(cutting_path,circle_line_data,linear_points_data,bound
     min_x = min(main_contour, key=lambda item: item[0])[0]
     min_y = min(main_contour, key=lambda item: item[1])[1]
     width,height = bounding_box_size
-    try:
-        scale = width/(max_y - min_y)
-    except Exception as e:
-        print(e)
-        scale = width/(max_x - min_x)
+
+    if max_x == min_x and max_y == min_y:
+        raise ValueError("Błąd GCODE obrazu")
+    if max_x == min_x:
+        scale_x = None
+    else:
+        scale_x = width / (max_x - min_x)
+
+    if max_y == min_y:
+        scale_y = None
+    else:
+        scale_y = width / (max_y - min_y)
+
+    if scale_x is not None and scale_y is not None:
+        scale = max(scale_x, scale_y)
+    elif scale_x is None:
+        scale = scale_y
+    else:
+        scale = scale_x
+
     dx = scale
     dy = scale
     output_res_x = width
@@ -148,20 +163,31 @@ def singleGcodeElementCV2(cutting_path,circle_line_data,linear_points_data,bound
         "linearData": adjusted_linear_data,
         "circleData": adjusted_circle_data,
     }
-    cv2.imshow("single",thresh)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     return gcode_data_packed
 
 def singleGcodeTest():
     (cutting_paths, _, _, _, _, sheet_size_line, circleLineData,
      linearPointsData) = visualize_cutting_paths_extended('../../../../Gcode to image conversion/NC_files/8.NC')
+    median_background_frame = capture_median_frame()
+    cv2.imshow("bgr",median_background_frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     for key, value in cutting_paths.items():
-        gcode_data = singleGcodeElementCV2(cutting_paths[key],circleLineData[key],linearPointsData[key],(500,500))
+        camera_image, bounding_box, img_pack = cameraImage(median_background_frame)
+        gcode_data = singleGcodeElementCV2(cutting_paths[key],circleLineData[key],linearPointsData[key],bounding_box)
+        is_ok = linesContourCompare(camera_image,gcode_data)
+        cv2.imshow(key + "camera", camera_image)
+        cv2.imshow(key, gcode_data['image'])
+        for i in range(len(img_pack)):
+            cv2.imshow(f'{i}',img_pack[i])
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 def capture_median_frame():
     while True:
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(1)
+        # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
         if not cap.isOpened():
             print("Nie udało się otworzyć kamerki")
             continue
@@ -171,6 +197,7 @@ def capture_median_frame():
     frames = []
     while len(frames) < 10:
         ret, frame = cap.read()
+        frame = camera_calibration(frame)
         if not ret:
             continue
         frames.append(frame)
@@ -185,13 +212,37 @@ def cameraImage(median_background_frame):
     gray_frame = cv2.cvtColor(median_frame, cv2.COLOR_BGR2GRAY)
     gray_background  = cv2.cvtColor(median_background_frame,cv2.COLOR_BGR2GRAY)
     diff = cv2.absdiff(gray_background,gray_frame)
-    background_subtractor = cv2.createBackgroundSubtractorMOG2()
-    foreground_mask = background_subtractor.apply(gray_frame)
-    _, thresholded = cv2.threshold(foreground_mask, 200, 255, cv2.THRESH_BINARY)
+    _, thresholded = cv2.threshold(diff, 70, 150, cv2.THRESH_BINARY)
     #odszumienie
     kernel = np.ones((5, 5), np.uint8)
     cleaned_thresholded = cv2.morphologyEx(thresholded, cv2.MORPH_OPEN, kernel)
-    return cleaned_thresholded
+    contours, _ = cv2.findContours(cleaned_thresholded, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    x, y, w, h = cv2.boundingRect(contours[0])
+    crop = cleaned_thresholded[y:y + h, x:x + w]
+    binarised = cv2.threshold(crop,50,255,cv2.THRESH_BINARY)[1]
+    cv2.imshow("gray_image",gray_frame)
+    cv2.imshow("diff",diff)
+    cv2.imshow('thr',cleaned_thresholded)
+    cv2.imshow("binarised",binarised)
+    cv2.imshow("przetworzony", crop)
+    img_pack = [median_frame,gray_frame,diff,cleaned_thresholded,crop]
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return binarised , (w,h), img_pack
+
+def camera_calibration(frame):
+    # Wczytanie parametrów kamery z pliku
+    loaded_mtx = np.loadtxt('../../../settings/mtx_matrix.txt', delimiter=',')
+    loaded_dist = np.loadtxt('../../../settings/distortion_matrix.txt', delimiter=',')
+    loaded_newcameramtx = np.loadtxt('../../../settings/new_camera_matrix.txt', delimiter=',')
+    loaded_roi = np.loadtxt('../../../settings/roi_matrix.txt', delimiter=',')
+
+    # Kalibracja kamery
+    frame = cv2.undistort(frame, loaded_mtx, loaded_dist, None, loaded_newcameramtx)
+    x, y, w, h = map(int, loaded_roi)
+    frame = frame[y:y + h, x:x + w]
+    return frame
 
 def imageBInfoExtraction(imageB):
     """
@@ -304,12 +355,9 @@ def linesContourCompare(imageB,gcode_data):
         contours, _ = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
         cornersB, contoursB, contourBImage, _, _ = imageBInfoExtraction(imageB)
         ret = cv2.matchShapes(contours[0],contoursB[0],1,0.0)
-        for contour in contours:
-            for contourB in contoursB:
-                ret = cv2.matchShapes(contour, contourB, 1, 0.0)
-                if ret > 0.02:
-                    print(f'Odkształcenie, ret: {ret} \n')
-                    return False
+        if ret > 0.05:
+            print(f'Odkształcenie, ret: {ret} \n')
+            return False
         gcodeLines = {
             "circle": gcode_data['circleData'],
             "linear": [],
@@ -544,7 +592,9 @@ def testAdditionalHole(imageB,gcode_data):
 
     return image_copy, linesContourCompare(image_copy,gcode_data)
 
+
 if __name__ == "__main__":
+
     singleGcodeTest()
     # Test czy spakowana funkcja działa
     # images, pts, sheet_size, pts_hole, circleLineData, linearData = allGcodeElementsCV2(
